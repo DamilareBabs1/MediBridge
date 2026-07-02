e0e4a9b7ccee5067576945a565e4ac5f5f0287cb
DamilareBabs1 <fashinababatunde200@gmail.com>
2026-07-02 15:32:02 +0100

    Fix asset import case

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -24,7 +24,7 @@ export default function Login() {
 
   const onSubmit = (data: LoginValue) => {
     console.log("Form Submitted :", data)
-    navigate('/')
+    navigate('/DashBoardLand')
   }
 
   return (
